ccadb99b308ce5022d89e3f2e00ae458d318aa92
Jonathan <contact@grafikart.fr>
2024-01-25 22:24:55 +0100

    ref: leap to flash

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -1,27 +1,16 @@
 return { -- Fast jump to a caracter using "s" or "S"
 {
-    "ggandor/leap.nvim",
-    enabled = true,
-    keys = {{
-        "s",
-        mode = {"n", "x", "o"},
-        desc = "Leap forward to"
-    }, {
-        "S",
-        mode = {"n", "x", "o"},
-        desc = "Leap backward to"
-    }, {
-        "gs",
-        mode = {"n", "x", "o"},
-        desc = "Leap from windows"
-    }},
-    config = function(_, opts)
-        local leap = require("leap")
-        for k, v in pairs(opts) do
-            leap.opts[k] = v
-        end
-        leap.add_default_mappings(true)
-        vim.keymap.del({"x", "o"}, "x")
-        vim.keymap.del({"x", "o"}, "X")
-    end
-}, }
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  }
+}
